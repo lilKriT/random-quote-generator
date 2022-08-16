d5f249340bf975748f676cbf8db0d4454ffa3e0e
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-16 22:05:31 +0200

    Fetching data.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const URL = "http://api.quotable.io/random;";
+
+const newQuoteButton = document.querySelector("button#new-quote");
+
+const fetchQuote = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+};
+
+newQuoteButton.addEventListener("click", fetchQuote);
