3bce4ffcb694a91cb2fdff6fee85fe6c66b9f119
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-17 18:54:25 +0200

    More styling. Fixed an error.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const URL = "http://api.quotable.io/random;";
+const URL = "https://api.quotable.io/random";
+const authorsURL = "https://api.quotable.io/authors";
 
 const newQuoteButton = document.querySelector("button#new-quote");
-const quote = document.querySelector("p.quote");
-const author = document.querySelector("span#author");
+// const quote = document.querySelector("p.quote");
+// const author = document.querySelector("span#author");
 
 const fetchQuote = async () => {
+  newQuoteButton.textContent = "Waiting...";
+  newQuoteButton.classList.add("inactive");
+
   const response = await fetch(URL);
   const data = await response.json();
-  console.log(data);
+  if (response.ok) {
+    newQuoteButton.textContent = "New Quote";
+    newQuoteButton.classList.remove("inactive");
+  } else {
+    console.log("Error", data);
+  }
 };
 
 newQuoteButton.addEventListener("click", fetchQuote);
